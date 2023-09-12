dec71911ad3e0b4fcdc56356b6184e9b0b8c1445
Leo Ribeiro <111469440+Leoribeiro61@users.noreply.github.com>
2023-09-11 21:13:44 -0300

    Add files via upload

---
+++ list.py
@@ -0,0 +1,62 @@
+#list 
+'''
+
+lista = []
+print(lista)
+
+type(lista)
+print(type(lista))
+lista = ['Diego', 'Maria']
+print(lista)
+
+lista = ['Joao', 'Jose']
+print(lista)
+
+lista = ['Joao', 3, 5, 4.8]
+print(lista)
+
+lista = ['Joao', 3, 5, lista]
+print(lista)
+'''
+lista = ['Diego', 'Maria']
+# C - Criar jose
+lista.append('Jose')
+print(lista)
+
+## R - Lista elemento específico pelo índice 
+print(lista[0])
+
+## U - Atualiza o nome Diego para Diogo 
+lista[0] = 'Diogo'
+print(lista)
+
+## D - 
+#del lista[0]
+#lista.pop(1)
+#lista.remove('Diogo')
+print(lista)
+
+
+lista.append('Jose')
+lista.append('Jose2')
+lista.append('Jose3')
+lista.append('Jose4')
+lista.append('Jose5')
+lista.append('Jose6')
+lista.append('Jose7')
+##L
+print(lista)
+print(lista[2:7])
+
+'''
+colecao = []
+colecao = ()
+colecao = {}
+List ? Diego, Maria, Diogo 
+Set ? Diogo, Maria
+Dict ? {1:Diogo, 2:Maria, 3:Diogo} chave: valor
+
+Dict? {"Nome": 'Diego', "Matricula": '010102', "endereco": 'Teste de teste'}
+
+{"Nome": 'Diego', "Matricula": '010102', "notas": 'notas'}
+'''
---
+++ teste.py
@@ -0,0 +1,11 @@
+#for i in range(9, -1, -1):
+#    print(i)
+
+for i in range(10):
+    print(f'valor{i}')
+
+for _ in range(10):
+    print('A frase que a tia pediu...')
+
+#for i in range(10):
+#numero = int(input('informe o numero:))
